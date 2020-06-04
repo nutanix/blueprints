db3af1207aaaa7fe2479aea6087f513935434e1d
Charan teja <charan@nutanix.com>
2020-06-04 15:08:24 +0530

    Update task library script (#28)
    * Initial commit to take args
    
    * Update script to seed the community and stable scripts
    
    * address review comments

--- calm-integrations/generate_task_library_items/generate_task_library.py
+++ calm-integrations/generate_task_library_items/generate_task_library.py
@@ -1,79 +1,153 @@
 """
 This script is used to create task-library items,
-that can be pushed to pc if ip and creds are passed to the script.
+that can be pushed to pc if ip, creds and project are passed to the script.
 
-This script takes script .sh, .es as input
-files only (.sh for shell scripts & .es for escripts).
+This script takes script .sh, .es, ps1, py as input
+files only (.sh for shell scripts, .es,.py for escripts, ps1 for powershell).
 
-Please pass file name as first argument.
+Pass file name as first argument.
+
+Usage:
+    python generate_task_library.py --pc 10.44.19.140 --user admin --password password --project default --script /root/script.ps1
+
 """
 import os
 import sys
 import json
 import ntpath
+import logging
+import argparse
+import requests
 
-script_path = sys.argv[1]
-project_name = "default"
-project_uuid = "7ffa7e0c-0929-41b7-bb30-4dfe836e8435"
+headers = {'content-type': 'application/json', 'Accept': 'application/json'}
 
-script_content = ""
 
-try:
-    scriptf = open(script_path, "r")
-except IOError:
-    print("ERROR: File not found.")
-    sys.exit(1)
-finally:
-    script_content = scriptf.read()
+def help_parser():
+    parser = argparse.ArgumentParser(
+        description='Arguments for seeding task library')
+    parser.add_argument('--pc',
+                        required=True,
+                        action='store',
+                        help='PC to connect to')
+    parser.add_argument('--port',
+                        type=int,
+                        default=9440,
+                        action='store',
+                        help='Port to connect on')
+    parser.add_argument('--user',
+                        required=True,
+                        action='store',
+                        help='User name to use when connecting to pc')
+    parser.add_argument('--password',
+                        required=True,
+                        action='store',
+                        help='Password to use when connecting to pc')
+    parser.add_argument('-p', '--project',
+                        required=True,
+                        action='store',
+                        help='Project to which task library item is created ')
+    parser.add_argument('-s', '--script',
+                        required=True,
+                        action='store',
+                        help='Script path')
+    return parser
+# --------------------------------------------------------------------------------- #
 
-script_name = ntpath.basename(script_path)
-item_name = os.path.splitext(script_name.replace("_", ' '))[0]
-script_type = os.path.splitext(script_name)[1].replace('.', '')
 
-payload = {
-  "api_version": "3.0",
-  "metadata": {
-    "kind": "app_task",
-    "project_reference": {
-      "kind": "project",
-      "name": project_name,
-      "uuid": project_uuid
+def get_project_uuid(base_url, auth, project_name):
+    method = 'POST'
+    url = base_url + "/projects/list"
+    payload = {
+        "length": 100,
+        "offset": 0,
+        "filter": "name=={0}".format(project_name)
     }
-  },
-  "spec": {
-    "name": item_name,
-    "resources": {
-      "variable_list": [],
-      "attrs": {
-        "script": script_content,
-        "script_type": script_type
+    resp = requests.request(
+        method,
+        url,
+        data=json.dumps(payload),
+        headers=headers,
+        auth=(auth["username"], auth["password"]),
+        verify=False
+    )
+
+    if resp.ok:
+        json_resp = resp.json()
+        if json_resp['metadata']['total_matches'] > 0:
+            project = json_resp['entities'][0]
+            project_uuid = project["metadata"]["uuid"]
+            return project_uuid
+        else:
+            logging.error("Could not find project")
+            sys.exit(-1)
+    else:
+        logging.error("Request failed")
+        logging.error("Headers: {}".format(headers))
+        logging.error('Status code: {}'.format(resp.status_code))
+        logging.error('Response: {}'.format(json.dumps(json.loads(resp.content), indent=4)))
+        sys.exit(-1)
+
+# --------------------------------------------------------------------------------- #
+
+
+def seed_task_item(base_url, auth, project_name, script_path):
+    print("INFO: Path of script '{}'.".format(script_path))
+    script_content = ""
+    try:
+        scriptf = open(script_path, "r")
+    except IOError:
+        print("ERROR: File not found.")
+        sys.exit(1)
+    finally:
+        script_content = scriptf.read()
+
+    script_name = ntpath.basename(script_path)
+    item_name = os.path.splitext(script_name.replace("_", ' '))[0]
+    script_ext = os.path.splitext(script_name)[1].replace('.', '')
+    project_uuid = get_project_uuid(base_url, auth, project_name)
+    if 'ps1' in script_ext:
+        script_type = "npsscript"
+    elif 'es' or 'py' in script_ext:
+        script_type = "static"
+    else:
+        script_type = "sh"
+
+    payload = {
+      "api_version": "3.0",
+      "metadata": {
+        "kind": "app_task",
+        "project_reference": {
+          "kind": "project",
+          "name": project_name,
+          "uuid": project_uuid
+        }
       },
-      "type": "EXEC"
-    },
-    "description": ""
-  }
-}
-
-pc_ip = "10.0.0.0"
-pc_username = "admin"
-pc_password = "secret"
-
-if pc_ip and pc_username and pc_password:
-    import requests
-
-    url = "https://{}:9440/api/nutanix/v3/app_tasks".format(pc_ip)
+      "spec": {
+        "name": item_name,
+        "resources": {
+          "variable_list": [],
+          "attrs": {
+            "script": script_content,
+            "script_type": script_type
+          },
+          "type": "EXEC"
+        },
+        "description": ""
+      }
+    }
+    print("INFO: Started Preseeding of task '{}'.".format(item_name))
+    url = base_url + "/app_tasks"
     request_payload = payload
-    headers = {'content-type': "application/json"}
     response = requests.request(
             "POST",
             url,
             data=json.dumps(request_payload),
             headers=headers,
-            auth=(pc_username, pc_password),
+            auth=(auth["username"], auth["password"]),
             verify=False
             )
     if response.status_code != 200:
-        print("ERROR: Unable to preseed task library item '{}': {}.".format(
+        print("ERROR: Unable to preseed task '{}': {}.".format(
             item_name, response.content)
             )
         sys.exit(1)
@@ -82,4 +156,18 @@ if pc_ip and pc_username and pc_password:
             item_name, response.json()["message_list"][0]["message"])
             )
         sys.exit(1)
-    print("INFO: Preseeded task library '{}'.".format(item_name))
+    print("INFO: Finish Preseeding of task '{}'.".format(item_name))
+
+
+if __name__ == "__main__":
+    parser = help_parser().parse_args()
+    pc_ip = parser.pc
+    pc_port = parser.port
+    script_path = parser.script
+    project_name = "default"
+    project = parser.project
+
+    base_url = "https://{}:{}/api/nutanix/v3".format(pc_ip, str(pc_port))
+    auth = {"username": parser.user, "password": parser.password}
+
+    seed_task_item(base_url, auth, project, script_path)
--- calm-integrations/generate_task_library_items/generate_task_library_items.sh
+++ calm-integrations/generate_task_library_items/generate_task_library_items.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
-export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+##############################################
+# Usage: bash generate_task_library_items.sh
+##############################################
 
-for items in `ls $(pwd)/../` ; do
-    python generate_task_library.py $(pwd)/../${items}
+export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+IFS=$(echo -en "\n\b")
+
+if [[ -z "${PC_IP}" ]]; then
+   read -p "Enter Prism Central IP: " pc_ip
+else
+   pc_ip=${PC_IP}
+fi
+if [[ -z "${PC_USER}" ]]; then
+   read -p "Enter Prism Central User: " pc_user
+else
+   pc_user=${PC_USER}
+fi
+if [[ -z "${PC_PASSWORD}" ]]; then
+   read -s -p "Enter Prism Central Password: " pc_password
+else
+   pc_password=${PC_PASSWORD}
+fi
+if [[ -z "${PC_PROJECT}" ]]; then
+   read -p "Enter Prism Central Project: " pc_project
+else
+   pc_project=${PC_PROJECT}
+fi
+
+#Seed nutanix calm published scripts
+for items in `find  ../../library/task-library -type f -print` ; do
+    python generate_task_library.py --pc $pc_ip --user $pc_user --password $pc_password --project $pc_project --script $items
+done
+
+#Seed community published scripts
+for items in `find  ../../task-library -type f \( -name "*.ps1" -o -name "*.sh" -o -name "*.py" \) -print` ; do
+    python generate_task_library.py --pc $pc_ip --user $pc_user --password $pc_password --project $pc_project --script $items
 done
