13517c097485b6778a6b7f9223180c262bfabd6b
Chris <chris@digitalformula.net>
2020-06-20 01:50:53 +1000

    Add README for seed script w/ link to official docs, add informative message about how many scripts will be imported, "fixed" missing carriage return after password prompt, flake8-ified a couple of things (#32)

--- calm-integrations/generate_task_library_items/generate_task_library_items.sh
+++ calm-integrations/generate_task_library_items/generate_task_library_items.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##############################################
-# Usage: bash generate_task_library_items.sh
+# Usage: bash generate_task_library_items.sh #
 ##############################################
 
 export PYTHONWARNINGS="ignore:Unverified HTTPS request"
@@ -19,6 +19,9 @@ else
 fi
 if [[ -z "${PC_PASSWORD}" ]]; then
    read -s -p "Enter Prism Central Password: " pc_password
+   # echo blank line to cleanup terminal
+   # using "-s" stops BASH from displaying \n after enter password
+   echo ""
 else
    pc_password=${PC_PASSWORD}
 fi
@@ -28,12 +31,19 @@ else
    pc_project=${PC_PROJECT}
 fi
 
+# grab some info about how many scripts will be imported
+# not mandatory but just gives the user an idea of how much there is to do
+PUBLISHED_SCRIPT_COUNT=`find  ../../library/task-library -type f -print | wc | awk '{print $1}'`
+COMMUNITY_SCRIPT_COUNT=`find  ../../task-library -type f \( -name "*.ps1" -o -name "*.sh" -o -name "*.py" \) -print | wc | awk '{print $1}'`
+
 #Seed nutanix calm published scripts
+echo "$PUBLISHED_SCRIPT_COUNT published scripts to import."
 for items in `find  ../../library/task-library -type f -print` ; do
     python generate_task_library.py --pc $pc_ip --user $pc_user --password $pc_password --project $pc_project --script $items
 done
 
 #Seed community published scripts
+echo "$COMMUNITY_SCRIPT_COUNT community scripts to import."
 for items in `find  ../../task-library -type f \( -name "*.ps1" -o -name "*.sh" -o -name "*.py" \) -print` ; do
     python generate_task_library.py --pc $pc_ip --user $pc_user --password $pc_password --project $pc_project --script $items
 done
